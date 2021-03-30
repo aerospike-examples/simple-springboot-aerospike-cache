9947e43021073bf5c595155bc8d2236fb9853f71
roimenashe <roimenashe5@gmail.com>
2021-03-30 13:02:04 +0300

    1. Add configuration properties. 2. Code cleanup.

--- src/main/java/com/aerospike/cache/simplespringbootaerospikecache/configuration/AerospikeConfiguration.java
+++ src/main/java/com/aerospike/cache/simplespringbootaerospikecache/configuration/AerospikeConfiguration.java
@@ -3,6 +3,7 @@ package com.aerospike.cache.simplespringbootaerospikecache.configuration;
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.policy.ClientPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,18 +15,21 @@ import org.springframework.data.aerospike.mapping.AerospikeMappingContext;
 import org.springframework.data.mapping.model.SimpleTypeHolder;
 
 @Configuration
+@EnableConfigurationProperties(AerospikeConfigurationProperties.class)
 @Import(value = {MappingAerospikeConverter.class, AerospikeMappingContext.class, AerospikeTypeAliasAccessor.class
-        , AerospikeCustomConversions.class, SimpleTypeHolder.class})
+        ,AerospikeCustomConversions.class, SimpleTypeHolder.class})
 public class AerospikeConfiguration {
 
     @Autowired
     private MappingAerospikeConverter mappingAerospikeConverter;
+    @Autowired
+    private AerospikeConfigurationProperties aerospikeConfigurationProperties;
 
     @Bean(destroyMethod = "close")
     public AerospikeClient aerospikeClient() {
         ClientPolicy clientPolicy = new ClientPolicy();
         clientPolicy.failIfNotConnected = true;
-        return new AerospikeClient(clientPolicy, "localhost", 3000);
+        return new AerospikeClient(clientPolicy, aerospikeConfigurationProperties.getHost(), aerospikeConfigurationProperties.getPort());
     }
 
     @Bean
---
+++ src/main/java/com/aerospike/cache/simplespringbootaerospikecache/configuration/AerospikeConfigurationProperties.java
@@ -0,0 +1,13 @@
+package com.aerospike.cache.simplespringbootaerospikecache.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "aerospike")
+public class AerospikeConfigurationProperties {
+    private String host;
+    private int port;
+}
