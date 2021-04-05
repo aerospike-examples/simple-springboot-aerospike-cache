00bcb74da8e3220f4bb43bbd4d7191de260b265a
roimenashe <roimenashe5@gmail.com>
2021-04-05 11:34:24 +0300

    Change from tabs to spaces (for github gists).

--- src/main/java/com/aerospike/cache/simplespringbootaerospikecache/SimpleSpringbootAerospikeCacheApplication.java
+++ src/main/java/com/aerospike/cache/simplespringbootaerospikecache/SimpleSpringbootAerospikeCacheApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class SimpleSpringbootAerospikeCacheApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringbootAerospikeCacheApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootAerospikeCacheApplication.class, args);
+    }
 }
